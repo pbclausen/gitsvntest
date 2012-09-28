4790eb353c3729bac741cfa0d962cd15aad03dc2
pbclausen <pbclausen@81502e52-820d-7f40-a103-204d0f396a6c>
2012-09-28 06:32:17 +0000

    Unit test timeout update
    git-svn-id: http://bksvn/svn/Instrumentation/trunk/Source/Dsp/KISS/KissXi/PCComm/Examples@288322 81502e52-820d-7f40-a103-204d0f396a6c

--- Matlab/WeighingAccumulateSpectrumTest.m
+++ Matlab/WeighingAccumulateSpectrumTest.m
@@ -11,7 +11,7 @@ try
     echo on
 
     % Set parameters
-    samples = 8192;
+    samples = 256;
     Fs = 48000;
 
     %macros.RandomAnalysis.SingleChannelConfiguration(0, Shared.Constants.CHANNELTYPE.CHANNELTYPE_CONTROL, 1.0, Shared.Constants.CONTROLSTRATEGY.CONTROLSTRATEGY_SINGLE, 1024, 0, Shared.Constants.WINDOWS.WINDOWSHANNING);
@@ -19,17 +19,23 @@ try
     channelTypes.Set(0, Shared.Constants.CHANNELTYPE.CHANNELTYPE_CONTROL);
     channelTypes.Set(1, Shared.Constants.CHANNELTYPE.CHANNELTYPE_CONTROL);
     channelTypes.Set(2, Shared.Constants.CHANNELTYPE.CHANNELTYPE_CONTROL);
+%    channelTypes.Set(3, Shared.Constants.CHANNELTYPE.CHANNELTYPE_CONTROL);
 
     %macros.RandomAnalysis.MultiChannelConfiguration([5, 1, 3], channelTypes, [0.7, 1.2, 0.5], Shared.Constants.CONTROLSTRATEGY.CONTROLSTRATEGY_WEIGHTING, samples, 0, Shared.Constants.WINDOWS.WINDOWSHANNING);
-    macros.RandomAnalysis.MultiChannelConfiguration([0, 3, 4], channelTypes, [0.8, 1.0, 1.5], Shared.Constants.CONTROLSTRATEGY.CONTROLSTRATEGY_WEIGHTING, samples, 0, Shared.Constants.WINDOWS.WINDOWSHANNING);
+    %macros.RandomAnalysis.MultiChannelConfiguration([2, 3, 5, 4], channelTypes, [0.9, 1.0, 1.5, 1.2], Shared.Constants.CONTROLSTRATEGY.CONTROLSTRATEGY_WEIGHTING, samples, 0, Shared.Constants.WINDOWS.WINDOWSHANNING);
+    macros.RandomAnalysis.MultiChannelConfiguration([0, 3, 5], channelTypes, [1.2, 0.9, 1.5], Shared.Constants.CONTROLSTRATEGY.CONTROLSTRATEGY_WEIGHTING, samples, 0, Shared.Constants.WINDOWS.WINDOWSHANNING);
     macros.RandomAnalysis.AlgorithmTestConfig(Shared.Constants.ALGORITHMTEST.ALGORITHMTEST_WEIGHTACCUMULATE);
 
     %macros.RandomAnalysis.SendHarmonic(1, samples, Fs, 2000);%10000);
-    macros.RandomAnalysis.SendHarmonic(0, samples, Fs, 2000);%10000);
+    %%macros.RandomAnalysis.SendHarmonic(2, samples, Fs, 2000);%10000);
     %macros.RandomAnalysis.SendHarmonic(5, samples, Fs, 5000);
-    macros.RandomAnalysis.SendHarmonic(3, samples, Fs, 5000);
+    %%macros.RandomAnalysis.SendHarmonic(3, samples, Fs, 3000);
     %macros.RandomAnalysis.SendHarmonic(3, samples, Fs, 3000);%7000);
-    macros.RandomAnalysis.SendHarmonic(4, samples, Fs, 3000);%7000);
+    %%macros.RandomAnalysis.SendHarmonic(5, samples, Fs, 6000);%7000);
+    %%macros.RandomAnalysis.SendHarmonic(4, samples, Fs, 3000);%7000);
+    macros.RandomAnalysis.SendHarmonic(0, samples, Fs, 3000);
+    macros.RandomAnalysis.SendHarmonic(3, samples, Fs, 6000);
+    macros.RandomAnalysis.SendHarmonic(5, samples, Fs, 9000);
     replyItem = macros.ItemController.GetItem();
     dataItem = Shared.Items.FloatData(replyItem);
 
